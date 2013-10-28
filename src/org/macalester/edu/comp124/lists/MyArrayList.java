bc1e4e8a1ac333934807df429d224bc9290d795b
mfinegold28 <sfinegol@macalester.edu>
2013-10-28 11:33:37 -0500

    Lists

--- src/org/macalester/edu/comp124/lists/MyArrayList.java
+++ src/org/macalester/edu/comp124/lists/MyArrayList.java
@@ -3,103 +3,119 @@ package org.macalester.edu.comp124.lists;
 
 /**
  * An unfinished implementation of a List.
- * 
+ * <p/>
  * TODO:
  * 1. Complete add(o) (unit test 1 and 2 should pass)
  * 2. Complete expand() (unit tests 1, 2, and 3 should pass)
  * 3. Complete add(i, o) (all 4 unit tests should pass)
- * 
+ *
  * @param <E>
  */
 public class MyArrayList<E> {
-	/**
-	 * The array elements
-	 */
-	private E elements[];
-	
-	/**
-	 * The number of elements currently contained in the list.
-	 */
-	private int currentSize;
+    /**
+     * The array elements
+     */
+    private E elements[];
 
-	/**
-	 * Creates a new list.
-	 */
-	public MyArrayList() {
-		clear();
-	}
-	
-	/**
-	 * Clears the contents of the array.
-	 */
-	public void clear() {
-		elements = newArrayOfE(4);
-		currentSize = 0;
-	}
-	
-	/**
-	 * Returns the current size of the array.
-	 * @return
-	 */
-	public int size() {
-		return currentSize;
-	}
-	
-	/**
-	 * Returns the element at position index.
-	 * @param index
-	 * @return
-	 */
-	public E get(int index) {
-		return elements[index];
-	}
-	
-	/**
-	 * Adds a new element to the end of the array:
-	 * 
-	 * If the array is full, expand the array.
-	 * Add the element to the first unused position in the array.
-	 * Update the size of the array.
-	 * 
-	 * @param elem
-	 */
-	public void add(E elem) {
-	}
+    /**
+     * The number of elements currently contained in the list.
+     */
+    private int currentSize;
 
-	/**
-	 * Adds a new element to the end of the array:
-	 * 
-	 * If the array is full, expand the array.
-	 * Slide the elements of the array starting with index over.
-	 * Place the new element in the correct spot of the array.
-	 * 
-	 * Update the size of the array.
-	 * 
-	 * @param elem
-	 */
-	public void add(int index, E elem) {
-	}
-	
-	/**
-	 * Doubles the size of the array, copies the old elements
-	 * into the new array, and updates elements to point to the
-	 * new array.
-	 * 
-	 * This will be useful for both versions of add.
-	 */
-	private void expandSize() {
-		
-	}
-	
-	/**
-	 * Due to some crazy Java constraints, we can't simply create a
-	 * new array of elements of class E.  We need to create an array
-	 * of class Object and then cast the array to E.
-	 * @param capacity
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	private E[] newArrayOfE(int capacity) {
-		return (E[]) (E[])new Object[capacity];
-	}
+    /**
+     * Creates a new list.
+     */
+    public MyArrayList() {
+        clear();
+    }
+
+    /**
+     * Clears the contents of the array.
+     */
+    public void clear() {
+        elements = newArrayOfE(4);
+        currentSize = 0;
+    }
+
+    /**
+     * Returns the current size of the array.
+     *
+     * @return
+     */
+    public int size() {
+        return currentSize;
+    }
+
+    /**
+     * Returns the element at position index.
+     *
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        return elements[index];
+    }
+
+    /**
+     * Adds a new element to the end of the array:
+     * <p/>
+     * If the array is full, expand the array.
+     * Add the element to the first unused position in the array.
+     * Update the size of the array.
+     *
+     * @param elem
+     */
+    public void add(E elem) {
+        if (currentSize == elements.length) {
+            expandSize();
+        }
+        elements[currentSize] = elem;
+        currentSize++;
+    }
+
+    /**
+     * Adds a new element to the end of the array:
+     * <p/>
+     * If the array is full, expand the array.
+     * Slide the elements of the array starting with index over.
+     * Place the new element in the correct spot of the array.
+     * <p/>
+     * Update the size of the array.
+     *
+     * @param elem
+     */
+    public void add(int index, E elem) {
+        if (currentSize == elements.length) {
+            expandSize();
+        }
+    }
+
+    /**
+     * Doubles the size of the array, copies the old elements
+     * into the new array, and updates elements to point to the
+     * new array.
+     * <p/>
+     * This will be useful for both versions of add.
+     */
+    private void expandSize() {
+        E[] newElements = newArrayOfE(2 * elements.length);
+        for (int i = 0; i < elements.length; i++) {
+
+        }
+
+
+    }
+
+    /**
+     * Due to some crazy Java constraints, we can't simply create a
+     * new array of elements of class E.  We need to create an array
+     * of class Object and then cast the array to E.
+     *
+     * @param capacity
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    private E[] newArrayOfE(int capacity) {
+        return (E[]) (E[]) new Object[capacity];
+    }
 }
--- tests/org/macalester/edu/comp124/lists/TestList.java
+++ tests/org/macalester/edu/comp124/lists/TestList.java
@@ -7,61 +7,61 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestList {
-	/* Declare a list of Strings here named list */
-	MyLinkedList<String> list = new MyLinkedList<String>();	
+    /* Declare a list of Strings here named list */
+    MyArrayList<String> list = new MyArrayList<String>();
 
-	public void prepareList() {
-		list.clear();
-		list.add("foo");
-		list.add("bar");
-		list.add("baz");
-	}
+    public void prepareList() {
+        list.clear();
+        list.add("foo");
+        list.add("bar");
+        list.add("baz");
+    }
 
     @Test
-	public void testGet() {
+    public void testGet() {
         prepareList();
-		assertEquals(list.get(0), "foo");
-		assertEquals(list.get(1), "bar");
-		assertEquals(list.get(2), "baz");
-	}
-	
-	/**
-	 * Tests that appending at the end of a list works.
-	 */
+        assertEquals(list.get(0), "foo");
+        assertEquals(list.get(1), "bar");
+        assertEquals(list.get(2), "baz");
+    }
+
+    /**
+     * Tests that appending at the end of a list works.
+     */
     @Test
-	public void testAdd1() {
+    public void testAdd1() {
         prepareList();
-		assertEquals(list.size(), 3);
-		assertEquals(list.get(0), "foo");
-		list.add("foobar");
-		assertEquals(list.size(), 4);
-		assertEquals(list.get(3), "foobar");
-	}
-	
-	/**
-	 * Tests that inserting into the middle of a list works.
-	 */
+        assertEquals(list.size(), 3);
+        assertEquals(list.get(0), "foo");
+        list.add("foobar");
+        assertEquals(list.size(), 4);
+        assertEquals(list.get(3), "foobar");
+    }
+
+    /**
+     * Tests that inserting into the middle of a list works.
+     */
     @Test
-	public void testAdd2() {
+    public void testAdd2() {
         prepareList();
-		assertEquals(list.size(), 3);
-		assertEquals(list.get(0), "foo");
-		list.add(1, "foobar");
-		assertEquals(list.size(), 4);
-		assertEquals(list.get(1), "foobar");
-		assertEquals(list.get(3), "baz");
-	}
-	
-	/**
-	 * Tests that expanding a list works.
-	 */
+        assertEquals(list.size(), 3);
+        assertEquals(list.get(0), "foo");
+        list.add(1, "foobar");
+        assertEquals(list.size(), 4);
+        assertEquals(list.get(1), "foobar");
+        assertEquals(list.get(3), "baz");
+    }
+
+    /**
+     * Tests that expanding a list works.
+     */
     @Test
-	public void testAdd3() {
+    public void testAdd3() {
         prepareList();
-		for (int i = 0; i < 100; i++) {
-			list.add("foo" + i);
-		}
-		assertEquals(list.size(), 103);
-		assertEquals(list.get(102), "foo99");
-	}
+        for (int i = 0; i < 100; i++) {
+            list.add("foo" + i);
+        }
+        assertEquals(list.size(), 103);
+        assertEquals(list.get(102), "foo99");
+    }
 }
